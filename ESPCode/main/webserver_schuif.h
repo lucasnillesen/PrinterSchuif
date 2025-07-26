c1f15a3fb25cea3af1f19205ac7873881edb6a41
lucasnillesen <lnillesen@qing.nl>
2025-07-26 16:00:58 +0200

    ESP code toegevoegd aan Git

---
+++ ESPCode/main/webserver_schuif.h
@@ -0,0 +1,10 @@
+#ifndef WEBSERVER_SCHUIF_H
+#define WEBSERVER_SCHUIF_H
+
+void setupWebServer();
+void handleWebServer();
+void handleSchuif();
+void schuifKlaarMelden(bool success);
+void schuifStarted();
+
+#endif
